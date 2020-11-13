6e3d1d9628788226d8a75cdb8a1f0e87cb7b69e2
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2020-11-13 17:28:13 -0600

    Chapter 1: Basic input and output

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -1,3 +1,31 @@
+use std::io::stdin;
+
+fn what_is_your_name() -> String {
+    let mut your_name = String::new();
+    stdin()
+        .read_line(&mut your_name)
+        .expect("Failed to read line");
+    your_name
+        .trim()
+        .to_lowercase()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let visitor_list = ["bert", "steve", "fred"];
+    let mut allow_them_in = false;
+
+    println!("Hello, what's your name?");
+    let name = what_is_your_name();
+
+    for visitor in &visitor_list {
+        if visitor == &name {
+            allow_them_in = true;
+        }
+    }
+
+    if allow_them_in {
+        println!("Welcome to the treehouse, {}", name);
+    } else {
+        println!("Sorry, you aren't in the list.");
+    }
 }
