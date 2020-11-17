74ccea964f19d3741cb810d9c3b4d9161688c4c0
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2020-11-17 12:53:32 -0600

    Chapter 3: Obstacles

--- flappy/src/main.rs
+++ flappy/src/main.rs
@@ -2,8 +2,10 @@ use bracket_lib::prelude::*;
 
 struct State {
     player: Player,
-    mode: GameMode,
     frame_time: f32,
+    obstacle: Obstacle,
+    mode: GameMode,
+    score: i32,
 }
 
 impl GameState for State {
@@ -21,7 +23,9 @@ impl State {
         State {
             player: Player::new(5, 25),
             frame_time: 0.0,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
             mode: GameMode::Menu,
+            score: 0,
         }
     }
 
@@ -35,9 +39,18 @@ impl State {
         if let Some(VirtualKeyCode::Space) = ctx.key {
             self.player.flap();
         }
+
         self.player.render(ctx);
+        self.obstacle.render(ctx, self.player.x);
+
         ctx.print(0, 0, "Press SPACE to flap");
-        if self.player.y > SCREEN_HEIGHT {
+        ctx.print(0, 1, &format!("Score: {}", self.score));
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+        }
+
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
             self.mode = GameMode::End;
         }
     }
@@ -117,6 +130,56 @@ impl Player {
 
     fn flap(&mut self) {
         self.velocity = -2.0;
+    }
+}
+
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32,
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score),
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
+        let screen_x = self.x - player_x;
+        let half_size = self.size / 2;
+
+        for y in 0..self.gap_y - half_size {
+            ctx.set(
+                screen_x,
+                y,
+                RGB::named(RED),
+                RGB::named(BLACK),
+                to_cp437('/'),
+            );
+        }
+
+        for y in self.gap_y + half_size..SCREEN_HEIGHT {
+            ctx.set(
+                screen_x,
+                y,
+                RGB::named(RED),
+                RGB::named(BLACK),
+                to_cp437('/'),
+            );
+        }
+    }
+
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        let does_x_match = player.x == self.x;
+        let player_above_gap = player.y < self.gap_y - half_size;
+        let player_below_gap = player.y > self.gap_y + half_size;
+        does_x_match && (player_above_gap || player_below_gap)
     }
 }
 
